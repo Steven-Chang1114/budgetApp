ef47f198ba6fba9bb5253e308dfea54fd4ad3032
Steven Chang <1632460272@qq.com>
2019-12-23 19:27:06 +0000

    Reading data from the input

--- app.js
+++ app.js
@@ -4,8 +4,29 @@ var budgetController = (function(){
 
 var UICntroller = (function(){
 
+    return{
+        getInput: function(){
+            return{
+                type: document.querySelector(".add__type").value,
+                description: document.querySelector(".add__description").value,
+                value: document.querySelector(".add__value").value
+            }
+        }
+    }
+
 })()
 
 var controller = (function(budget, ui){
+
+    function ctrlAddItem(){
+        var input = ui.getInput();
+    }
+
+    document.querySelector(".add__btn").addEventListener("click", ctrlAddItem) ;
+    document.addEventListener("keypress", function(event){
+        if(event.keyCode == 13 && event.which == 13){
+            ctrlAddItem();
+        }
+    });
 
 })(budgetController, UICntroller)
