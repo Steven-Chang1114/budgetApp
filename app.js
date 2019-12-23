caa50fb6d90068a50ce43bc8ae708b7f8eda40f3
Steven Chang <1632460272@qq.com>
2019-12-23 21:49:39 +0000

    Creation initializations

--- app.js
+++ app.js
@@ -1,32 +1,65 @@
+//Budget
 var budgetController = (function(){
 
 })()
 
+
+//UI
 var UICntroller = (function(){
+
+    //All the string name for class/id for DOM
+    var DOMstr = {
+        inputType: ".add__type",
+        inputDescription: ".add__description",
+        inputValue: ".add__value",
+        inputBtn: ".add__btn"
+    };
 
     return{
         getInput: function(){
             return{
-                type: document.querySelector(".add__type").value,
-                description: document.querySelector(".add__description").value,
-                value: document.querySelector(".add__value").value
+                type: document.querySelector(DOMstr.inputType).value,
+                description: document.querySelector(DOMstr.inputDescription).value,
+                value: document.querySelector(DOMstr.inputValue).value
             }
+        },
+
+        getDOMstr: function(){
+            return DOMstr;
         }
+
     }
 
 })()
 
+
+//Global control
 var controller = (function(budget, ui){
+
+    function setUpEventListeners(){
+         
+        var DOM = ui.getDOMstr();
+
+        //Event listener
+        document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem) ;
+        document.addEventListener("keypress", function(event){
+            if(event.keyCode == 13 && event.which == 13){
+                ctrlAddItem();
+            }
+        });
+    }
 
     function ctrlAddItem(){
         var input = ui.getInput();
     }
 
-    document.querySelector(".add__btn").addEventListener("click", ctrlAddItem) ;
-    document.addEventListener("keypress", function(event){
-        if(event.keyCode == 13 && event.which == 13){
-            ctrlAddItem();
+    return{
+        init: function(){
+             setUpEventListeners();
         }
-    });
+    }
 
 })(budgetController, UICntroller)
+
+//Onle line of code outside of the controllers
+controller.init();
